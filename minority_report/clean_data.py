1ad9d77002b3b41f6b7c43a76ee12f51ba166559
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-12-02 13:50:32 +0100

    commenting out completed to boolean function, replaced by label encoding in scalingf later

--- minority_report/clean_data.py
+++ minority_report/clean_data.py
@@ -379,14 +379,14 @@ class CleanData:
       return self.data
 
     # 10. Run complete_to_boolean sur df['crime_completed']
-    def crime_completed_to_boolean(self):
-      """
-          turns complete/incomplete into boolean value
-      """
-      df = self.data.copy()
-      df['crime_completed'] = df['crime_completed'].replace({'COMPLETED': True, 'INCOMPLETE': False})
-      self.data = df
-      return self.data
+    # def crime_completed_to_boolean(self):
+    #   """
+    #       turns complete/incomplete into boolean value
+    #   """
+    #   df = self.data.copy()
+    #   df['crime_completed'] = df['crime_completed'].replace({'COMPLETED': True, 'INCOMPLETE': False})
+    #   self.data = df
+    #   return self.data
 
 
     def total_clean(self):
