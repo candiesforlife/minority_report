9b5eafcb5e65044a0cddee5256542812bbbe6c42
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-12-02 15:50:37 +0100

    scaling class working with if name=='main' woop woopppppppp

--- minority_report/scaling.py
+++ minority_report/scaling.py
@@ -14,19 +14,32 @@ class Scaling:
         self.data = CleanData().total_clean()
 
     def label_encoding(self):
+        '''
+        Scaling column crime_completed with label_encoder technique. Returns the df updated.
+        '''
         le = LabelEncoder()
         self.data['crime_completed'] = le.fit_transform(self.data['crime_completed'])
         return self.data
 
     def one_hot_encoding(self,features_list):
+        '''
+        Scaling categorical columns with one_hot_encoder technique. Returns the df updated.
+        '''
         ohe = OneHotEncoder(sparse = False)
         ohe.fit(self.data[features_list])
         list_of_list_of_columns = [list(element) for element in ohe.categories_]
         columns = list(itertools.chain(*list_of_list_of_columns))
         et_oh = ohe.transform(self.data[features_list])
         self.data[columns] = et_oh
-        # self.data = clean_data.data
         return self.data
+
+    def save_data(self, file_name):
+      '''
+      Saves encoded clean dataframe as a csv file.
+      '''
+      root_dir = os.path.dirname(os.path.dirname(__file__))
+      csv_path = os.path.join(root_dir, 'raw_data', f'{file_name}.csv')
+      return self.data.to_csv(csv_path, index = False)
 
 if __name__ == '__main__':
   print('initializing sclaing object with clean data')
@@ -36,4 +49,8 @@ if __name__ == '__main__':
   print('One hot encoding')
   features_list = ['offense_type','offense_level','borough','premise_desc','premise','suspect_age','suspect_race','suspect_sex','patrol_borough', 'metro','victim_age','victim_race','victim_sex','precinct_number']
   df.one_hot_encoding(features_list)
-  print('finished with scaling buddies')
+  print('Saving the encoding data as a csv file')
+  df.save_data('data_encoded')
+  print('finished!')
+
+
