7d1695187a22ef19f8e8b3382fae7245ce3a39a9
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-12-05 18:28:12 +0100

    putting all the function coord, matrix to the right class => geoimg class

--- minority_report/geo_img.py
+++ minority_report/geo_img.py
@@ -33,6 +33,91 @@ class GeoImg:
         self.data = df
         return self.data
 
+    def from_meters_to_coords(self,lat_meters, lon_meters):
+        """
+        gives the latitude and longitude step to use for the grid buckets
+        lat_meters, lon_meters = lat/lon step
+        """
+        #Position, decimal degrees
+        lat = 40
+        lon = -73
+
+        #Earth’s radius, sphere
+        R=6378137
+
+        #offsets in meters
+        dn = lat_meters
+        de = lon_meters
+
+        #Coordinate offsets in radians
+        dLat = dn/R
+        dLon = de/(R*np.cos(np.pi*lat/180))
+
+        #OffsetPosition, decimal degrees
+        latO = dLat * 180/np.pi
+        lonO = dLon * 180/np.pi
+
+        del lat, lon, R, dn, de, dLat, dLon #pour recuperer de la memoire dans le notebook
+
+        return latO, lonO
+
+    def from_coord_to_matrix(self, lat_meters, lon_meters):
+        """
+        outputs the 3D matrix of all coordinates for a given bucket height and width in meters
+        """
+        df = self.data.copy()
+        #add 'time_index' column to df
+        ind = {time:index for index,time in enumerate(np.sort(df['period'].unique()))}
+
+        df['time_index'] = df['period'].map(ind)
+        self.data = df
+        #initiate matrix
+        grid_offset = np.array([0, -40.91553277600008,  -74.25559136315213]) # Where do you start
+        #from meters to lat/lon step
+        lat_spacing, lon_spacing = self.from_meters_to_coords(lat_meters, lon_meters )
+        grid_spacing = np.array([1, lat_spacing , lon_spacing]) # What's the space you consider (euclidian here)
+
+
+        #get points coordinates
+        coords = np.array([(t_ind, -lat, lon) for t_ind, lat, lon \
+                       in zip(df['time_index'],df['latitude'],df['longitude'])])
+
+
+        # Convert point to index
+        indexes = np.round((coords - grid_offset)/grid_spacing).astype('int')
+        Z = indexes[:,0]
+        Y = indexes[:,1]
+        X = indexes[:,2]
+
+        #virgin matrix
+        a = np.zeros((Z.max()+1, Y.max()+1, X.max()+1))
+
+
+        a[Z, Y, X] = 1
+
+        del ind, grid_offset, lat_spacing, lon_spacing, grid_spacing, coords, indexes, Z, Y, X
+
+        return a, a.shape[1], a.shape[2]
+
+
+    def from_matrix_to_coord(self,indexes, lat_meters, lon_meters):
+        """
+        gives back the coordinates from a 3D matrix for a given bucket height and width
+        """
+        df = self.data.copy()
+
+        # Where do you start
+        grid_offset = np.array([0, -40.91553277600008,  -74.25559136315213,])
+
+        #from meters to lat/lon step
+        lat_spacing, lon_spacing = self.from_meters_to_coords(df,lat_meters, lon_meters)
+
+        # What's the space you consider (euclidian here)
+        grid_spacing = np.array([1, lat_spacing, lon_spacing])
+
+        result = grid_offset + indexes * grid_spacing
+        return result
+
 
     def plotting_img3D(self): #data viz check
         img3D= np.zeros((743,50,50))
@@ -57,19 +142,24 @@ class GeoImg:
 
 
 if __name__ == '__main__':
-  print('initializing geo class')
+  print('1. Creating an instance of GeoImg class')
   df = GeoImg()
-  print("loading data")
+
+  print("2. Loading data")
   df.load_data()
-  # print('get a sample of a month-time crimes grouped by hour')
-  # lats_per_image, lons_per_image = df.group_by_hour(2016, 12, 17)
-  # print('Transforming into geoseries thanks to geopandas')
-  # df.get_geoseries(lat, lon)
-  # print('saving images to numpy array')
-  # images_np_array = df.save_img_to_np_array(lats_per_image,lons_per_image)
-  img3D = np.zeros((743,50,50))
-  img3D[15,15,15] = 1
-  print('applying gaussian filter to np_array')
-  df.gaussien_filtering(img3D, 2,2,2)
+
+  print('3. From meters to coordinates ')
+  lat_meters = 100
+  lon_meters = 100
+  df.from_meters_to_coords(lat_meters, lon_meters)
+
+  print('4. From coordinates to matrix')
+  img3D = df.from_coord_to_matrix(lat_meters, lon_meters)
+
+  # print('5. From matrix to coordinates')
+  # df.from_matrix_to_coord(indexes, lat_meters, lon_meters)
+
+  print('5. Gaussian filtering')
+  df.gaussian_filtering(img3D, 2,2,2)
   print('Finished!')
 
