aa94bb29791f990e87079f3b004a4bb86683f597
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-12-05 17:48:18 +0100

    function from meters to coords to matrix to coords

--- minority_report/input.py
+++ minority_report/input.py
@@ -1,6 +1,7 @@
 
 import os
 import pandas as pd
+import numpy as np
 import pickle
 
 class Input:
@@ -33,6 +34,89 @@ class Input:
         X = np.array(X)
         y = np.array(y)
         return X, y
+
+    def from_meters_to_coords(lat_meters, lon_meters):
+        """
+        gives the latitude and longitude step to use for the grid buckets
+        lat_meters, lon_meters = lat/lon step
+        """
+        #Position, decimal degrees
+        lat = 40
+        lon = -73
+
+        #Earth’s radius, sphere
+        R=6378137
+
+        #offsets in meters
+        dn = lat_meters
+        de = lon_meters
+
+        #Coordinate offsets in radians
+        dLat = dn/R
+        dLon = de/(R*np.cos(np.pi*lat/180))
+
+        #OffsetPosition, decimal degrees
+        latO = dLat * 180/np.pi
+        lonO = dLon * 180/np.pi
+
+        del lat, lon, R, dn, de, dLat, dLon
+
+        return latO, lonO
+
+    def from_coord_to_matrix(df, lat_meters, lon_meters):
+        """
+        outputs the 3D matrix of all coordinates for a given bucket height and width in meters
+        """
+        df=df.copy()
+        #add 'time_index' column to df
+        ind = {time:index for index,time in enumerate(np.sort(df['period'].unique()))}
+        df['time_index'] = df['period'].map(ind)
+
+        #initiate matrix
+        grid_offset = np.array([0, -40.91553277600008,  -74.25559136315213,]) # Where do you start
+        #from meters to lat/lon step
+        lat_spacing, lon_spacing = from_meters_to_coords(lat_meters, lon_meters )
+        grid_spacing = np.array([1, lat_spacing , lon_spacing]) # What's the space you consider (euclidian here)
+
+
+        #get points coordinates
+        coords = np.array([(t_ind, -lat, lon) for t_ind, lat, lon \
+                       in zip(df['time_index'],df['latitude'],df['longitude'])])
+
+
+        # Convert point to index
+        indexes = np.round((coords - grid_offset)/grid_spacing).astype('int')
+        Z = indexes[:,0]
+        Y = indexes[:,1]
+        X = indexes[:,2]
+
+        #virgin matrix
+        a = np.zeros((Z.max()+1, Y.max()+1, X.max()+1))
+
+
+        a[Z, Y, X]=1
+
+        del ind, grid_offset, lat_spacing, lon_spacing, grid_spacing, coords, indexes, Z, Y, X
+
+        return a, a.shape[1], a.shape[2]
+
+
+    def from_matrix_to_coord(indexes, lat_meters, lon_meters):
+        """
+        gives back the coordinates from a 3D matrix for a given bucket height and width
+        """
+        # Where do you start
+        grid_offset = np.array([0, -40.91553277600008,  -74.25559136315213,])
+
+        #from meters to lat/lon step
+        lat_spacing, lon_spacing = from_meters_to_coords(lat_meters, lon_meters )
+
+        # What's the space you consider (euclidian here)
+        grid_spacing = np.array([1, lat_spacing, lon_spacing])
+
+        result = grid_offset + indexes * grid_spacing
+        return result
+
 
 
 if __name__ == '__main__':
