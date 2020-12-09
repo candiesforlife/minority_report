049a4df36f79b8030867ee8ae7801b7bd49a3806
Ellyn Bouscasse <ellyn.bouscasse@gmail.com>
2020-12-09 23:23:29 +0100

    commenting out the rehape in fit_model and evaluate_model and making it a clean function

--- minority_report/trainer.py
+++ minority_report/trainer.py
@@ -56,43 +56,43 @@ class Trainer:
         return self.X_train, self.X_test, self.y_train, self.y_test
 
     def init_model(self):
-          print('init model')
-          self.model = models.Sequential()
+        print('init model')
+        self.model = models.Sequential()
 
-          print('3D conv 1')
-          self.model.add(layers.Conv3D(32, kernel_size = (4,4,4), activation = 'relu', padding='same',
-                                     input_shape = (256, 256, 18,1))) # 256 and 256 are mandatory but the nb of sheets of X (18) must be changed accordingly with the input
+        print('3D conv 1')
+        self.model.add(layers.Conv3D(32, kernel_size = (4,4,4), activation = 'relu', padding='same',
+                                 input_shape = (256, 256, 18,1))) # 256 and 256 are mandatory but the nb of sheets of X (18) must be changed accordingly with the input
         self.model.add(layers.MaxPooling3D(2))
 
-          print('3D conv 2')
-          self.model.add(layers.Conv3D(128, kernel_size = (3,3,3), activation = 'relu', padding='same'))
-          self.model.add(layers.MaxPooling3D(2))
+        print('3D conv 2')
+        self.model.add(layers.Conv3D(128, kernel_size = (3,3,3), activation = 'relu', padding='same'))
+        self.model.add(layers.MaxPooling3D(2))
 
-          print('3D conv 3')
-          self.model.add(layers.Conv3D(64, kernel_size = (2,2,2), activation = 'relu', padding='same'))
-          self.model.add(layers.MaxPooling3D(2))
+        print('3D conv 3')
+        self.model.add(layers.Conv3D(64, kernel_size = (2,2,2), activation = 'relu', padding='same'))
+        self.model.add(layers.MaxPooling3D(2))
 
-          print('3D conv 4')
-          self.model.add(layers.Conv3D(16, kernel_size = (2,2,2), activation = 'relu', padding='same'))
-          self.model.add(layers.MaxPooling3D(2))
+        print('3D conv 4')
+        self.model.add(layers.Conv3D(16, kernel_size = (2,2,2), activation = 'relu', padding='same'))
+        self.model.add(layers.MaxPooling3D(2))
 
-          self.model.add(layers.Flatten())
-          self.model.add(layers.Dense(256))
-          self.model.add(layers.Reshape((16,16,1)))
+        self.model.add(layers.Flatten())
+        self.model.add(layers.Dense(256))
+        self.model.add(layers.Reshape((16,16,1)))
 
-          self.model.add(layers.UpSampling3D(size=(2, 2, 2))) #size[2] must be changed accordingly with the nb of sheets in y (change with other layers)
-          self.model.add(layers.UpSampling3D(size=(2, 2, 2)))
-          self.model.add(layers.UpSampling3D(size=(2, 2, 1)))
-          self.model.add(layers.UpSampling3D(size=(2, 2, 1)))
+        self.model.add(layers.UpSampling3D(size=(2, 2, 2))) #size[2] must be changed accordingly with the nb of sheets in y (change with other layers)
+        self.model.add(layers.UpSampling3D(size=(2, 2, 2)))
+        self.model.add(layers.UpSampling3D(size=(2, 2, 1)))
+        self.model.add(layers.UpSampling3D(size=(2, 2, 1)))
 
-          self.model.compile(loss ='mse',
-                     optimizer='adam',
-                     metrics='mae')
-          return model
+        self.model.compile(loss ='mse',
+                 optimizer='adam',
+                 metrics='mae')
+        return model
 
 
     def fit_model(self,batch_size, epochs, patience):
-        self.X_train = self.X_train.reshape(-1, self.X_train.shape[1], self.X_train.shape[2], self.X_train.shape[3], 1)
+        # self.X_train = self.X_train.reshape(-1, self.X_train.shape[1], self.X_train.shape[2], self.X_train.shape[3], 1)
         es = EarlyStopping(patience = patience, restore_best_weights=True)
         self.model.fit(self.X_train, self.y_train,
                       batch_size = batch_size,
@@ -102,7 +102,7 @@ class Trainer:
         return self.model
 
     def evaluate_model(self):
-        self.X_test = self.X_test.reshape(-1, self.X_test.shape[1], self.X_test.shape[2], self.X_test.shape[3], 1)
+        # self.X_test = self.X_test.reshape(-1, self.X_test.shape[1], self.X_test.shape[2], self.X_test.shape[3], 1)
         result = self.model.evaluate(self.X_test, self.y_test)
         return result
 
