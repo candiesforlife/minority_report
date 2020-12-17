f282fe41440c2b90f4c876d6870e2202ad02ea34
Alice de Bokay <alicedebokay@googlemail.com>
2020-12-17 20:24:57 +0100

    Dropped unnecessary columns

--- minority_report/pre_clean.py
+++ minority_report/pre_clean.py
@@ -5,8 +5,33 @@ import pandas as pd
 
 from minority_report.data import NYPD
 
+
 class PreClean:
 
     def __init__(self):
-      # loads core dataframe
+      '''
+      Loads core dataframe
+      '''
       self.data = NYPD().get_data()
+
+    def drop_cols(self):
+        '''
+        Drops unnecessary columns, such as IDs
+        '''
+        df = self.data.copy()
+
+        df.drop(columns = ['CMPLNT_NUM',
+                        'CMPLNT_TO_DT',
+                        'CMPLNT_TO_TM',
+                        'HADEVELOPT',
+                        'HOUSING_PSA',
+                        'JURISDICTION_CODE',
+                        'KY_CD',
+                        'PD_CD',
+                        'PD_DESC',
+                        'RPT_DT',
+                        'TRANSIT_DISTRICT',
+                        'X_COORD_CD',
+                        'Y_COORD_CD',
+                        'Lat_Lon',
+                       ], inplace = True)
