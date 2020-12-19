c5321ee1568a34b3ab0f8c5bc6d030e6899c5d3f
Alice de Bokay <alicedebokay@googlemail.com>
2020-12-19 20:50:52 +0100

    Added pre-clean functions to clean_data.py

--- minority_report/clean_data.py
+++ minority_report/clean_data.py
@@ -14,6 +14,72 @@ class CleanData:
       # loads core dataframe
       self.data = NYPD().get_data()
 
+    def drop_cols(self):
+        '''
+        Drops unnecessary columns, such as IDs
+        '''
+        df = self.data.copy()
+
+        df.drop(columns = ['CMPLNT_NUM',
+                        'CMPLNT_TO_DT',
+                        'CMPLNT_TO_TM',
+                        'HADEVELOPT',
+                        'HOUSING_PSA',
+                        'JURISDICTION_CODE',
+                        'KY_CD',
+                        'PD_CD',
+                        'PD_DESC',
+                        'RPT_DT',
+                        'TRANSIT_DISTRICT',
+                        'X_COORD_CD',
+                        'Y_COORD_CD',
+                        'Lat_Lon',
+                       ], inplace = True)
+
+        self.data = df
+        return self.data
+
+    def rename_cols(self):
+        '''
+        Renames dataset columns to more readable names
+        '''
+
+        df = self.data.copy()
+
+        # dictionary of new names for each column
+        columns_dic = {
+
+            'CMPLNT_FR_DT' : 'date',
+            'CMPLNT_FR_TM' : 'time',
+
+            'LAW_CAT_CD' : 'offense_level',
+            'OFNS_DESC' : 'offense_type',
+            'CRM_ATPT_CPTD_CD' : 'crime_completed',
+
+            'LOC_OF_OCCUR_DESC' : 'premise_desc',
+            'PREM_TYP_DESC' : 'premise',
+            'Latitude' : 'latitude',
+            'Longitude' : 'longitude',
+            'BORO_NM' : 'borough',
+            'PATROL_BORO' : 'patrol_borough',
+            'ADDR_PCT_CD' : 'precinct_number',
+            'JURIS_DESC' : 'jurisdiction',
+
+            'SUSP_AGE_GROUP' : 'suspect_age',
+            'SUSP_RACE' : 'suspect_race',
+            'SUSP_SEX' : 'suspect_sex',
+            'VIC_AGE_GROUP' : 'victim_age',
+            'VIC_RACE' : 'victim_race',
+            'VIC_SEX' : 'victim_sex',
+            'PARKS_NM' : 'park_name',
+            'STATION_NAME' : 'metro'
+        }
+
+        df.rename(columns = columns_dic, inplace = True)
+
+        self.data = df
+        return self.data
+
     #  1.
     def drop_nan(self):
       '''
@@ -396,7 +462,7 @@ class CleanData:
       '''
       df = self.data.copy()
 
-      df.drop(columns = 'complaint_id', inplace = True)
+      #df.drop(columns = 'complaint_id', inplace = True)
 
       list_column = ['period',
                      'latitude',
@@ -441,7 +507,11 @@ class CleanData:
       '''
       Combines all cleaning functions and returns clean dataframe
       '''
-      print('dropping NaNs')
+      print('Dropping Columns')
+      self.drop_cols()
+      print('Renaming Columns')
+      self.rename_cols()
+      print('Dropping NaNs')
       self.drop_nan()
       print('Changing date column')
       self.to_date_format()
